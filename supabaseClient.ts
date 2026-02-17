c0cc1a9fb50e327c3cb5ffad14a3474e2fec9858
Inon Cohen <96375193+Inoncohen2@users.noreply.github.com>
2026-02-17 14:05:55 +0200

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://iehehxricvjedgzlhipi.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllaGVoeHJpY3ZqZWRnemxoaXBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzMDY5MTEsImV4cCI6MjA4NTg4MjkxMX0.Z4GsVn6DCgTA-B11tt1tj-zjaH-dx8TWJrN36yox8R0';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
